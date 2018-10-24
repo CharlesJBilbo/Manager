0d3439fc45c1cfbc3a4216a92cbe5edec7829de0
Unknown <cbilbo9@gmail.com>
2018-10-24 17:08:53 -0500

    added framework
    added framework

---
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Socialite;
+use Exception;
+use Auth;
+
+class SocialAuthController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        $existingUser = User::where('email', $user->email)->first();
+        if($existingUser){
+            // log them in
+            auth()->login($existingUser, true);
+            return redirect('/');
+        } else {
+        try {
+            $create                  = new User;
+            $create->name            = $user->name;
+            $create->email           = $user->email;
+            $create->google_id       = $user->id;
+            $create->avatar          = $user->avatar;
+            $create->avatar_original = $user->avatar_original;
+            $create->save();
+            Auth::loginUsingId($create->id);
+
+            return redirect('/');
+
+        } catch (Exception $e) {
+
+            return $e;
+
+        }
+    }
+}
+}
